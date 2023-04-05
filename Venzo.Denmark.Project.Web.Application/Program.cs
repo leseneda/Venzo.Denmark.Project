358e9afe491840f0b16bd93c71a9202f15c32a5a
Alexandre <Alexandre@DESKTOP-H9Q6DIT>
2023-04-06 00:47:30 +0100

    Added AddServices extension

--- Venzo.Denmark.Project.Web.Application/Program.cs
+++ Venzo.Denmark.Project.Web.Application/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Venzo.Denmark.Project.Web.Application;
+using Venzo.Denmark.Project.Web.Application.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
+
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddServices(builder.Configuration, builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
